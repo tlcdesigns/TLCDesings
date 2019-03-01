05d753d4f1b98edfc51a208fbf2da08896fdb05f
MichaelCapo23 <123pie23@gmail.com>
2019-03-01 12:39:41 -0800

    routed all pages created basic class components

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import '../assets/css/app.scss';
+import "./landing/styles.css"
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min'
+import 'material-icons';
 import logo from '../assets/images/logo.svg';
+import {Route} from "react-router-dom"
+import Buying from "./buying";
+import Landing from "./landing";
+import Profile from "./profile";
+import Search from "./search";
+import Cart from "./cart";
+import SignIn from "./sign/signin/signIn";
+import SignUp from "./sign/signUp/signUp";
+
 
 const App = () => (
     <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome To React</h1>
-        </div>
+        <Route exact path={"/"} component={Landing}/>
+        <Route path={"/search"} component={Search}/>
+        <Route path={"/buying"} component={Buying}/>
+        <Route path={"/cart/:userID"} component={Cart}/>
+        <Route path={"/userProfile/:userID"} component={Profile}/>
+        <Route path={"/signIn"} component={SignIn}/>
+        <Route path={"/signUp"} component={SignUp}/>
     </div>
 );
 
---
+++ src/components/buying/buying.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+
+
+class Buying extends Component{
+    render() {
+        return (
+            <div>
+                <h1 className="center">Buying Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Buying
