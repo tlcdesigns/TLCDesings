01d0083f2c45403def8f345bbd38ba8be01d51f2
MichaelCapo23 <123pie23@gmail.com>
2019-03-04 15:41:58 -0800

    chnaged color layout and added company name in nav

--- src/components/sign/signUp/signUp.js
+++ src/components/sign/signUp/signUp.js
@@ -1,14 +1,33 @@
 import React, {Component} from "react";
+import {Link} from "react-router-dom"
+import {connect} from "react-redux"
+import SignUpValidation from "./validateSignUp"
 
 
-class SignUp extends Component{
+class SignUp extends Component {
+    state = {
+        hasAttempted: false
+    }
+
     render() {
+        const {hasAttempted} = this.state;
         return (
             <div>
-                <h1 className="center">Sign Up Page</h1>
+                <h1 className="center">Sign Up</h1>
+                <SignUpValidation formSubmitFunction={this.signUserIn}/>
+                <p>{this.props.message && hasAttempted ? this.props.message : ""}</p>
+                <p className="col center-align l12 m12 s12">Already Have An Account? <Link className='textColor' to={"/SignIn"}>Sign In! </Link> </p>
             </div>
         )
     }
 }
 
-export default SignUp
+function mapStateTopRPops(state) {
+    return {
+
+    }
+}
+
+export default connect(mapStateTopRPops, {
+
+})(SignUp)
