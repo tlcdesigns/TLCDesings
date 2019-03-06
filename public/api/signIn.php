2a40bdbc6baaa756c71ee5acc40694e989ea1a92
MichaelCapo23 <123pie23@gmail.com>
2019-03-05 19:51:06 -0800

    sign up query

--- public/api/signIn.php
+++ public/api/signIn.php
@@ -21,15 +21,34 @@ $AccountID = null;
 $checkForUserQuery->store_result();
 $checkForUserQuery->bind_result($AccountID);
 $logUserIn = [];
+$token = date("h:i:sa");
 
 if($checkForUserQuery->num_rows>0) {
     while($checkForUserQuery->fetch()) {
-        $logUserIn['token'] = date("h:i:sa");
-        echo "got to fetch";
+        $logUserIn['token'] = $token;
     }
 } else {
     $logUserIn['error'] = "Invalid Email Or Password";
 }
+
+$addToLoggedinQuery = $conn->prepare("INSERT INTO `loggedin`
+                                            (accountID, token, login_time)
+                                            VALUES (?, ? , ?)");
+
+$addToLoggedinQuery -> bind_param("sss", $AccountID, $token, date('h:i:sa'));
+
+$addToLoggedinQuery->execute();
+$addToLoggedinQuery->bind_result();
+
+if($addToLoggedinQuery->num_rows>0) {
+    $logUserIn['loggedin'] = true;
+} else {
+    $logUserIn['loggedinError'] = "Unable to Add User To LoggedIn Table"
+}
+
+
+
+
 
 print_r(json_encode($logUserIn));
 
