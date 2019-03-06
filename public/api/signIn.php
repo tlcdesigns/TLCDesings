d76172d5981f07eebe8cc52dcfa3f13b556ff94f
MichaelCapo23 <123pie23@gmail.com>
2019-03-05 20:48:40 -0800

    install bcrypt for password storage, completed sign in endpoint

--- public/api/signIn.php
+++ public/api/signIn.php
@@ -1,5 +1,9 @@
 <?php
 require_once('connect.php');
+require '../../vendor/autoload.php';
+use Bcrypt\Bcrypt;
+$bcrypt = new Bcrypt();
+$bcrypt_version = '2a';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -10,18 +14,25 @@ $jsondata = json_decode($postdata, true);
 $email = $jsondata['Email'];
 $password = $jsondata['Password'];
 
+$passwordHash = $bcrypt->encrypt($password,$bcrypt_version);
+
 $checkForUserQuery = $conn->prepare("SELECT ID
                       FROM `accounts`
                       WHERE email = ?
                       AND password = ?");
 
-$checkForUserQuery -> bind_param("ss", $email, $password);
+$checkForUserQuery -> bind_param("ss", $email, $passwordHash);
 $checkForUserQuery->execute();
+
 $AccountID = null;
 $checkForUserQuery->store_result();
 $checkForUserQuery->bind_result($AccountID);
 $logUserIn = [];
-$token = date("h:i:sa");
+$tokenData = date("h:i:sa");
+$token = $bcrypt->encrypt($tokenData,$bcrypt_version);
+
+
+
 
 if($checkForUserQuery->num_rows>0) {
     while($checkForUserQuery->fetch()) {
@@ -32,22 +43,17 @@ if($checkForUserQuery->num_rows>0) {
 }
 
 $addToLoggedinQuery = $conn->prepare("INSERT INTO `loggedin`
-                                            (accountID, token, login_time)
-                                            VALUES (?, ? , ?)");
+                                            (accountID, token)
+                                            VALUES (?, ?)");
 
-$addToLoggedinQuery -> bind_param("sss", $AccountID, $token, date('h:i:sa'));
-
+$addToLoggedinQuery -> bind_param("ss", $AccountID, $token);
 $addToLoggedinQuery->execute();
-$addToLoggedinQuery->bind_result();
-
-if($addToLoggedinQuery->num_rows>0) {
+print_r($addToLoggedinQuery);
+if($addToLoggedinQuery) {
     $logUserIn['loggedin'] = true;
 } else {
-    $logUserIn['loggedinError'] = "Unable to Add User To LoggedIn Table"
+    $logUserIn['loggedinError'] = "Unable to Add User To LoggedIn Table";
 }
-
-
-
 
 
 print_r(json_encode($logUserIn));
