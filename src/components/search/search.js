61ed4b90e9f16351e45eb0f1487e07998296860f
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-03-05 23:30:07 -0800

    mapping through dummy data working

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -2,49 +2,21 @@ import React, {Component} from "react";
 import ListItem from './listings';
 import dummy from '../bummyData/dummy'
 
-// class Search extends Component{
+class Search extends Component {
 
-    // getListingData = (results) => {
-    //     const listItems = results.map((item,index)=>{
-    //         return(
-    //             <ListItem key={index} about={item}/>
-    //         )
-    //     });
-    //     return listItems
-    // }
-    // const listings = dummy.map((item,index)=>{
-    //     return (
-    //         <ListItem key={index} about={item}/>
-    //     )
-    // });
-
-  
-    // render() {
-        // let listings = this.getListingData()
-//         return (
-//             <div>
-//                 <h1 className="center">Search Page</h1>
-//                 <ListItem/>
-//                 <div>{listings}</div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default Search
-
-
-export default props => {
-    const listings = dummy.map((item,index)=>{
+    render() {
+        const listings = dummy.map((item,index)=>{
+            return (
+                <ListItem key={index} about={item}/>
+            )
+        });
         return (
-            <ListItem key={index} about={item}/>
+            <div>
+                <h1 className="center">Search Page</h1>
+                {/* <ListItem/> */}
+                <div>{listings}</div>
+            </div>
         )
-    });
-    return (
-        <div>
-            <h1 className="center">Search Page</h1>
-            {/* <ListItem/> */}
-            <div>{listings}</div>
-        </div>
-    )
-}
+    }
+ }
+ export default Search
