bff585b2a6a70765e6f194e85adeafa50a975682
Jordan Salisbury <salisburyjordan21@gmail.com>
2019-03-09 23:08:20 -0800

    f

--- src/components/itemDetails/itemDetails.js
+++ src/components/itemDetails/itemDetails.js
@@ -1,9 +1,17 @@
 import React, {Component} from "react";
 
 class ItemDetails extends Component {
+   
     render() {
+        const {Title, Description, image, price} = props.about
         return (
-            <div>Item Details</div>
+            <div>
+                    <div>Item Details</div>
+                    <h1>{Title}</h1>
+                    <p>{Description}</p>
+                    <h1>{price}</h1>
+            </div>
+            
         )
     }
 }
