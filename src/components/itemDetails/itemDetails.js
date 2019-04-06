c0e2dc1c457a2edb1e7cffa2bcc161b1f2275f67
MichaelCapo23 <123pie23@gmail.com>
2019-04-05 22:54:43 -0700

    itemdetails improvements

--- src/components/itemDetails/itemDetails.js
+++ src/components/itemDetails/itemDetails.js
@@ -8,12 +8,11 @@ import {addToCartAction} from "../../actions/addToCartAction"
 import topaz from '../search/images/topaz_necklace.jpg';
 
 
-
 class ItemDetails extends Component {
 
     state = {
         buttonClicked: false,
-        leftRating: false,
+        RatingButton: false,
     }
 
     addToCart = () => {
@@ -35,11 +34,23 @@ class ItemDetails extends Component {
     }
 
     MakeReview = () => {
+        this.reviewContainer
         this.setState({
-            leftRating: true
+            RatingButton: !this.state.RatingButton
         })
         console.log("review function ran!");
     }
+
+    maxLength = (e) => {
+        let max = e.target.attributes.maxLength.value;
+        e.onkeypress = function () {
+            if (this.value.length >= max) return false;
+        };
+    }
+
+    submitReview = () => {
+        console.log("submit review");
+    };
 
     render() {
         if (this.props.itemDetails) {
@@ -75,7 +86,7 @@ class ItemDetails extends Component {
                             </select>
                         </div>
                         <div className={"col s12 rowContainer"}>
-                            <div className={"col 3 quantityContainer"}>
+                            <div className={"col s2 quantityContainer"}>
                                 <label htmlFor="quantityDropDown">Quantity</label>
                                 <select ref={e => this.quantity = e} name="quantityDropDown" id="quantityDropDown">
                                     <option value="1">1</option>
@@ -90,23 +101,40 @@ class ItemDetails extends Component {
                                     <option value="10">10</option>
                                 </select>
                             </div>
+                            <div className={"col center s10 itemInCartContainer"}>
+                                {/*<div className={"col s2 picOfCart"}></div>*/}
+                                <div
+                                    className="col s12 center cartText">{`${300} other people have this in their carts.`}</div>
+                            </div>
                         </div>
                         <div className="col s12 addToCartContainer">
-                            <button onClick={this.addToCart} className={"col s12 checkoutBtn center btn"}>Add Item(s) To Cart</button>
+                            <button onClick={this.addToCart} className={"col s12 checkoutBtn center btn"}>Add To
+                                Cart
+                            </button>
                         </div>
                         <div className={"center"}>
                             {this.props.addToCartConfirmation && this.state.buttonClicked ? this.props.addToCartConfirmation : ""}
                         </div>
                         <div className="col s12 ReviewButtonContainer">
-                            <button onClick={this.MakeReview} className={"col s12 checkoutBtn center btn"}>Leave A Review</button>
+                            <button onClick={this.MakeReview} className={"col s12 checkoutBtn center btn"}>Leave A
+                                Review
+                            </button>
                         </div>
                         <div className={"center"}>
                             {this.state.leftRating ? 'Thank you for leaving a rating!' : ""}
                         </div>
-                        <div className={"col s12 itemInCartContainer"}>
-                            <div className={"col s2 picOfCart"}></div>
-                            <div className="col s10 center cartText">{`${300} other people have this in their carts.`}</div>
+
+                        <div ref={e => this.reviewContainer = e}
+                             className={this.state.RatingButton ? "col s12 reviewContainer" : "col s12 noReview"}>
+                            <div className={"col s12 starRating"}>
+                                <div className="col s12 center ratingText">{`Rating: stars here`}</div>
+                            </div>
+                            <textarea onChange={this.maxLength} maxLength={"300"} className={"comments"}
+                                      name="comments" id="comments" cols="30" rows="10"></textarea>
+                            <button onClick={this.submitReview} className={"col s12 checkoutBtn center btn"}>Submit Review</button>
                         </div>
+
+
                     </div>
                 </div>
 
